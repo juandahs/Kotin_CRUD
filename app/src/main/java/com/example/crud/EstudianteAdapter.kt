1c5004429417196a14ae9bdf938dbe5465b0d5c3
Juan David Castro Marin <jdcastro861@soy.sena.edu.co>
2024-11-15 18:50:12 -0500

    Se termina interfaz gráfica. creo...

--- app/src/main/java/com/example/crud/EstudianteAdapter.kt
+++ app/src/main/java/com/example/crud/EstudianteAdapter.kt
@@ -1,4 +1,46 @@
 package com.example.crud
 
-class EstudianteAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.crud.databinding.EstudianteBinding
+
+class EstudianteAdapter: RecyclerView.Adapter<EstudianteAdapter.EstudianteViewHolder>() {
+
+    private var estudiantes: ArrayList<EstudianteModel> = ArrayList()
+
+    inner class EstudianteViewHolder(itemview: View) : RecyclerView.ViewHolder(itemview) {
+        val binding = EstudianteBinding.bind(itemview)
+
+        fun bindViewEstudiante(estudiante: EstudianteModel) {
+            with(binding) {
+                tvNombreEstudiante.text = estudiante.nombre
+                tvCorreoEstudiante.text = estudiante.correoElectronico
+                tvCursoEstudiante.text = estudiante.curso
+            }
+        }
+    }
+
+//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EstudianteViewHolder {
+//        TODO("Not yet implemented")
+//    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = EstudianteViewHolder(
+        LayoutInflater.from(parent.context).inflate(R.layout.estudiante, parent, false)
+    )
+
+    override fun getItemCount(): Int = estudiantes.size
+
+    override fun onBindViewHolder(holder: EstudianteViewHolder, position: Int) {
+        val estudiante = estudiantes[position]
+        holder.bindViewEstudiante(estudiante)
+    }
+
+
+    fun addEstudiante(estudiantes: ArrayList<EstudianteModel>)
+    {
+        this.estudiantes = estudiantes
+        notifyDataSetChanged() //recarga el adaptador
+    }
 }
